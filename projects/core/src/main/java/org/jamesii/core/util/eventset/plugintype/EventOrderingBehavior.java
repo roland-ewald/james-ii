0a8f70eb16e391209bf9d9568667fa9431c43c28
Arne Bittig <arne.bittig@uni-rostock.de>
2014-03-03 16:05:49 +0100

    OPEN - issue JAMESII-423: Event queue cleanup, categorization & tests https://altair.informatik.uni-rostock.de/jira/browse/JAMESII-423 Expanded categories of EventOrderingBehavior, added test (which does not actually test the ordering behavior, as it is defined in the factory and the test is for event queue objects; thus it only logs the determined behavior to sysout)

--- projects/core/src/main/java/org/jamesii/core/util/eventset/plugintype/EventOrderingBehavior.java
+++ projects/core/src/main/java/org/jamesii/core/util/eventset/plugintype/EventOrderingBehavior.java
@@ -4,11 +4,10 @@ import org.jamesii.core.util.eventset.IEventQueue;
 
 /**
  * Describes how an {@link IEventQueue} implementation returns events with
- * identical time stamps.
- * 
- * This is important when using methods like
+ * identical time stamps. This is important when using methods like
  * {@link IEventQueue#dequeueAll(Comparable)}, as it defines which event order
- * to expect there.
+ * to expect there, but also for reproducibility of experiments using
+ * {@link IEventQueue#dequeue()}.
  * 
  * @author Arne Bittig
  * @author Roland Ewald
@@ -16,35 +15,73 @@ import org.jamesii.core.util.eventset.IEventQueue;
  */
 public enum EventOrderingBehavior {
 
-  /**
-   * First in, first out. This means that the order in which events are enqueued
-   * is preserved if their sorting criterion (here, sorting by time stamps)
-   * considers them to be equal, i.e., the sorting is stable.
-   */
-  FIFO(true),
+	/**
+	 * First in, first out. This means that the order in which events are
+	 * enqueued is preserved if their sorting criterion (here, sorting by time
+	 * stamps) considers them to be equal, i.e., the sorting is stable.
+	 */
+	FIFO(true, true),
 
-  /**
-   * Last in, first out. This means that the order in which events are enqueued
-   * is <b>reversed</b>. While not stable (see
-   * {@link EventOrderingBehavior#FIFO}), the event ordering is still
-   * deterministic.
-   */
-  LIFO(false),
+	/**
+	 * Last in, first out. This means that the order in which events are
+	 * enqueued is <b>reversed</b>. While not stable (see
+	 * {@link EventOrderingBehavior#FIFO}), the event ordering is still
+	 * deterministic.
+	 */
+	LIFO(false, true),
 
-  /**
-   * No assumptions can be made regarding the order in which events with the
-   * same time stamp are returned.
-   */
-  NO_ORDER(false);
+	/** Natural order of the event type, regardless of enqueue order */
+	NATURAL(false, true),
 
-  /** Flag to define whether the ordering behavior is stable. */
-  private final boolean stable;
+	/**
+	 * Same-time events are not returned in first-in-first-out,
+	 * last-in-first-out or natural order, but the return order is reproducible
+	 * for the same events and same sequence of enqueue operations.
+	 */
+	UNORDERED_REPRODUCIBLE(false, true),
+	/**
+	 * No assumptions can be made regarding the order in which events with the
+	 * same time stamp are returned.
+	 */
+	UNREPRODUCIBLE(false, false);
 
-  private EventOrderingBehavior(boolean stable) {
-    this.stable = stable;
-  }
+	/** Flag for ordering behavior stability, see {@link #isStable()}. */
+	private final boolean stable;
 
-  public boolean isStable() {
-    return stable;
-  }
+	/**
+	 * Flag for ordering behavior is reproducibility, see
+	 * {@link #isReproducible()}
+	 */
+	private final boolean reproducible;
+
+	private EventOrderingBehavior(boolean stable, boolean reproducible) {
+		this.stable = stable;
+		this.reproducible = reproducible;
+	}
+
+	/**
+	 * Indicate whether the sequence in which events with the same time stamp
+	 * are returned corresponds to the behavior of a stable sorting algorithm.
+	 * 
+	 * This is a strictly stronger condition than {@link #isReproducible()
+	 * reproducibility}.
+	 * 
+	 * @return true iff event queue preserves insertion order of same-time-stamp
+	 *         events
+	 */
+	public boolean isStable() {
+		return stable;
+	}
+
+	/**
+	 * Indicate whether event queue can be used to reproduce experiments that
+	 * include events with the same time stamp, or whether the event queue
+	 * introduces indeterminism in this case. (The latter can, for example, be
+	 * the result of using HashMaps or HashSets internally for these events.)
+	 * 
+	 * @return true iff return order of same-time-stamp is reproducible
+	 */
+	public boolean isReproducible() {
+		return reproducible;
+	}
 }
