b118572a30c89a8c8c760db59e157134911d3d87
re027 <devnull@localhost>
2014-03-03 15:04:29 +0100

    javadocs: some more fixes to the source documentation of MessageQueue

--- projects/core/src/main/java/org/jamesii/core/processor/messages/MessageQueue.java
+++ projects/core/src/main/java/org/jamesii/core/processor/messages/MessageQueue.java
@@ -11,19 +11,18 @@ import java.io.Serializable;
 import org.jamesii.core.util.collection.Fifo;
 
 /**
- * This class implements a first-in-first-out queue storing
- * messages. The queue's length dynamically increases and decreases. There must
- * be producers and consumers in order to put a message in or get a message out.
- * This buffer synchronizes producers and consumers in that way, that a producer
- * can write to the end of the queue if no other writer is active -- otherwise
- * or if a reader is active it will be blocked. A consumer can only read (and
- * thereby empty) the buffer, if something is in it -- otherwise the consumer is
- * blocked, till the buffer is not empty and no producer is active.
+ * This class implements a first-in-first-out queue to store messages. The
+ * queue's length dynamically increases and decreases. There must be producers
+ * and consumers in order to put a message in or get a message out. This buffer
+ * synchronizes producers and consumers so that a producer can write to the end
+ * of the queue if no other writer is active -- otherwise, or if a reader is
+ * active, the call will be blocked. A consumer can only read (and thereby
+ * empty) the buffer if something is in it -- otherwise the consumer is blocked,
+ * till the buffer is not empty and no producer is active.
  * 
  * @param <T>
- * 
- * @see org.jamesii.core.processor.messages.Message T, the type of the sender
- *      used in the messages stored in the queues
+ *          the generic type of {@link Message}, i.e. the type of the sender of
+ *          the messages stored in the queue
  * 
  */
 public final class MessageQueue<T> implements Serializable {
@@ -31,28 +30,27 @@ public final class MessageQueue<T> implements Serializable {
   /** The Constant serialVersionUID. */
   static final long serialVersionUID = -29089168598146023L;
 
-  // --- INTERNAL STRUCTURES ---------------------------------------------
-
   /** This first in first out queue is the condition variable. */
   private Fifo<IMessage<T>> fifo = new Fifo<>();
 
   /**
-   * used for the wake up of the thread who wait for the queue ,when some one
-   * want to force the end of this certain thread.
+   * Used to wake up the thread that waits for the queue. Useful, for example,
+   * to stop a thread.
    */
   private boolean finish = false;
 
-  /** The is waiting. */
+  /**
+   * The flag to determine whether a consumer thread is waiting for a message to
+   * arrive.
+   */
   private boolean isWaiting = false;
-
-  // --- BLOCKING SERVICE METHODS ----------------------------------------
 
   /**
    * Gets and deletes a message from the queue. If there is no message to get,
-   * the consumer (calling this method) is blocked until a producer puts a
+   * the consumer (calling this method) is blocked until a producer puts in a
    * message.
    * 
-   * @return the first message which is in the queue
+   * @return the first message in the queue
    */
   public synchronized IMessage<T> getMessage() {
     while (fifo.isEmpty()) {
@@ -67,9 +65,9 @@ public final class MessageQueue<T> implements Serializable {
   }
 
   /**
-   * check whether the queue is empty.
+   * Check whether the queue is empty.
    * 
-   * @return true, if checks if is empty
+   * @return true if queue is empty
    */
   public synchronized boolean isEmpty() {
     return fifo.isEmpty();
@@ -87,12 +85,13 @@ public final class MessageQueue<T> implements Serializable {
       } catch (InterruptedException e) {
       }
     }
+
     this.isWaiting = false;
+
     if (!finish) {
       return;
     }
     notifyAll();
-
   }
 
   /**
@@ -100,37 +99,33 @@ public final class MessageQueue<T> implements Serializable {
    * blocked as long as an other producer or consumer is active.
    * 
    * @param message
-   *          which should be put into the queue
+   *          message to be put into the queue
    */
   public synchronized void putMessage(IMessage<T> message) {
-
     fifo.add(message);
-
     notifyAll();
   }
 
   /**
    * Show top message.
    * 
-   * @return the i message<?>
+   * @return the first message in the queue
    */
   public synchronized IMessage<T> showTopMessage() {
-
     return fifo.get();
   }
 
   /**
    * Show size.
    * 
-   * @return the int
+   * @return the size of the queue
    */
   public synchronized int size() {
     return fifo.size();
   }
 
   /**
-   * Used to wake up of the thread who wait for the queue ,when some one
-   * want to force the end of this certain thread.
+   * Used to wake up the thread that waits for the queue (see {@link #finish}).
    */
   public synchronized void fairwell() {
     finish = true;
