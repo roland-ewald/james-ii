4706d8de416285c88de50147cc256a2aad681f1c
Arne Bittig <arne.bittig@uni-rostock.de>
2014-09-11 16:10:04 +0200

    deprecated OperationNotSupportedException, removed the two (of 4) contructors that are not used anywhere

--- projects/core/src/main/java/org/jamesii/core/util/exceptions/OperationNotSupportedException.java
+++ projects/core/src/main/java/org/jamesii/core/util/exceptions/OperationNotSupportedException.java
@@ -1,9 +1,7 @@
-/*
- * The general modelling and simulation framework JAMES II.
- * Copyright by the University of Rostock.
+/* The general modelling and simulation framework JAMES II. Copyright by the
+ * University of Rostock.
  * 
- * LICENCE: JAMESLIC
- */
+ * LICENCE: JAMESLIC */
 package org.jamesii.core.util.exceptions;
 
 /**
@@ -11,15 +9,17 @@ package org.jamesii.core.util.exceptions;
  * called (might be available because it has been defined in an interface the
  * class is implementing) to indicate that the called method is not supported by
  * the class.
- * 
+ *
  * This exception was added to confuse James II users who otherwise might be
  * confused by an {@link java.lang.UnsupportedOperationException} encountered
  * outside the "Java Collections Framework", as the latter's documentation
- * describes it to be "a member" thereof.
- * 
+ * describes it to be "a member" thereof. The latter is now preferred
+ * nevertheless.
+ *
  * @author Jan Himmelspach
- * 
+ *
  */
+@Deprecated
 public class OperationNotSupportedException extends RuntimeException {
 
   private static final long serialVersionUID = -996609121354059015L;
@@ -29,14 +29,6 @@ public class OperationNotSupportedException extends RuntimeException {
 
   public OperationNotSupportedException(String message) {
     super(message);
-  }
-
-  public OperationNotSupportedException(Throwable cause) {
-    super(cause);
-  }
-
-  public OperationNotSupportedException(String message, Throwable cause) {
-    super(message, cause);
   }
 
 }
